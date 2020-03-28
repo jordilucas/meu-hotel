6abfee7cbe7d6f19f28a418936f97f76450f1dea
Jordi <jordilucas3@gmail.com>
2020-03-28 13:57:46 -0300

    Rename MainActivity -> HotelActivity, and create dataclass Hotel

--- app/src/main/java/com/jordilucas/meuhotel/HotelActivity.kt
+++ app/src/main/java/com/jordilucas/meuhotel/HotelActivity.kt
@@ -3,7 +3,7 @@ package com.jordilucas.meuhotel
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class HotelActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
