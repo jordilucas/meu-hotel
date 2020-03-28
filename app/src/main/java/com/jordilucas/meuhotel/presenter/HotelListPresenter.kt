ed17c6f5295ca31c8fad0a640cc831b63bb9496b
Jordi <jordilucas3@gmail.com>
2020-03-28 14:46:48 -0300

    implementacao hotelPresenter

--- app/src/main/java/com/jordilucas/meuhotel/presenter/HotelListPresenter.kt
+++ app/src/main/java/com/jordilucas/meuhotel/presenter/HotelListPresenter.kt
@@ -1,4 +1,20 @@
 package com.jordilucas.meuhotel.presenter
 
-class HotelListPresenter{
+import com.jordilucas.meuhotel.data.Hotel
+import com.jordilucas.meuhotel.repository.HotelRepository
+import com.jordilucas.meuhotel.view.HotelListView
+
+class HotelListPresenter(
+    private val view:HotelListView,
+    private val repository: HotelRepository
+){
+    fun searchHotels(term:String) {
+        repository.search(term){hotels ->
+            view.showHotels(hotels)
+        }
+    }
+
+    fun showHotelDetails(hotel: Hotel){
+        view.showHotelDetail(hotel)
+    }
 }
