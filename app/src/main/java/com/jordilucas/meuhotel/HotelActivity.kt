10164ce60a970d8bce6d6a517a28e5e64be0cb79
Jordi <jordilucas3@gmail.com>
2020-03-29 20:38:43 -0300

    Layout para tablet

--- app/src/main/java/com/jordilucas/meuhotel/HotelActivity.kt
+++ app/src/main/java/com/jordilucas/meuhotel/HotelActivity.kt
@@ -2,21 +2,37 @@ package com.jordilucas.meuhotel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.jordilucas.meuhotel.data.Hotel
+import com.jordilucas.meuhotel.fragments.HotelDetailsFragment
 import com.jordilucas.meuhotel.fragments.HotelListFragment
 
 class HotelActivity : AppCompatActivity(), HotelListFragment.OnHotelClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_hotel)
+        setContentView(R.layout.activity_hotel)
     }
 
     override fun onHotelClick(hotel: Hotel) {
-        showDetailsHotel(hotel.id)
+        if(isTablet()){
+            showDetailsFragment(hotel.id)
+        }else {
+            showDetailsActivity(hotel.id)
+        }
     }
 
-    private fun showDetailsHotel(hotelId:Long){
+    private fun isTablet() = findViewById<View>(R.id.details) != null
+
+    private fun showDetailsFragment(hotelId:Long){
+        val fragment = HotelDetailsFragment.newInstance(hotelId)
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.details, fragment, HotelDetailsFragment.TAG_DETAILS)
+            .commit()
+    }
+
+    private fun showDetailsActivity(hotelId:Long){
         HotelDetailsActivity.open(this, hotelId)
     }
 
