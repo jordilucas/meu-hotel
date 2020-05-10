e051f9e4d652bf19aba796926924d0f740081c87
Jordi <jordilucas3@gmail.com>
2020-05-10 13:42:52 -0300

    ViewModel para salvar hotel

---
+++ app/src/main/java/com/jordilucas/meuhotel/form/HotelFormViewModel.kt
@@ -0,0 +1,24 @@
+package com.jordilucas.meuhotel.form
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import com.jordilucas.meuhotel.model.Hotel
+import com.jordilucas.meuhotel.repository.HotelRepository
+import com.jordilucas.meuhotel.repository.HotelValidator
+
+class HotelFormViewModel(private val repository: HotelRepository): ViewModel() {
+
+    private val validator by lazy { HotelValidator() }
+
+    fun loadHotel(id:Long):LiveData<Hotel>{
+        return repository.hotelById(id)
+    }
+
+    fun saveHotel(hotel:Hotel):Boolean{
+        return validator.validate(hotel)
+            .also { validated ->
+                if(validated)repository.save(hotel)
+            }
+    }
+
+}
