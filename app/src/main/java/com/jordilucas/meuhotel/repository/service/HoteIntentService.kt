38e454c96ef119edbf23f41e4460825a05ef65f6
Jordi <jordilucas3@gmail.com>
2020-07-22 16:03:26 -0300

    implementacao de service para sincronizar dados com servidor em background

---
+++ app/src/main/java/com/jordilucas/meuhotel/repository/service/HoteIntentService.kt
@@ -0,0 +1,29 @@
+package com.jordilucas.meuhotel.repository.service
+
+import android.content.Intent
+import androidx.core.app.JobIntentService
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
+import org.koin.android.ext.android.inject
+
+class HoteIntentService: JobIntentService() {
+    override fun onHandleWork(intent: Intent) {
+        val hotelHttp: HotelHttp by inject()
+        val it = Intent(ACTION_SYNC)
+        val lbm = LocalBroadcastManager.getInstance(this)
+        try {
+            hotelHttp.syncronizeWithServer()
+            it.putExtra(EXTRA_SUCCESS, true)
+        }catch (e:Exception){
+            it.putExtra(EXTRA_SUCCESS, false)
+            e.printStackTrace()
+        }finally {
+            lbm.sendBroadcast(it)
+        }
+    }
+
+    companion object{
+        const val ACTION_SYNC = "sync_hotels"
+        const val EXTRA_SUCCESS = "success"
+    }
+
+}
