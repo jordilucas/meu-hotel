77b6f63daf00c41a7086b7b2343885c495fe1151
Jordi <jordilucas3@gmail.com>
2020-07-22 11:34:25 -0300

    Refatoracao injecao de dependencia

--- app/src/main/java/com/jordilucas/meuhotel/di/AndroidModule.kt
+++ app/src/main/java/com/jordilucas/meuhotel/di/AndroidModule.kt
@@ -1,10 +1,13 @@
 package com.jordilucas.meuhotel.di
 
+import android.content.Context
 import com.google.gson.GsonBuilder
 import com.jordilucas.meuhotel.details.HotelDetailsViewModel
 import com.jordilucas.meuhotel.form.HotelFormViewModel
 import com.jordilucas.meuhotel.hotelList.HotelListViewModel
 import com.jordilucas.meuhotel.repository.HotelRepository
+import com.jordilucas.meuhotel.repository.imagefiles.FindHotelPicture
+import com.jordilucas.meuhotel.repository.imagefiles.ImageGalleryPictureFinder
 import com.jordilucas.meuhotel.repository.room.HotelDatabase
 import com.jordilucas.meuhotel.repository.room.RoomRepository
 import com.jordilucas.meuhotel.repository.service.HotelHttp
@@ -17,12 +20,27 @@ import org.koin.dsl.module.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-val androidModule = module{
-    single{this}
-    single{
-        RoomRepository(HotelDatabase.getDatabase(context = get())) as
-                HotelRepository
-
+val androidModule = module {
+    single { this }
+    single {
+        RoomRepository(HotelDatabase.getDatabase(context = get())) as HotelRepository
+    }
+    viewModel {
+        HotelListViewModel(repository = get())
+    }
+    viewModel {
+        HotelDetailsViewModel(repository = get())
+    }
+    viewModel {
+        HotelFormViewModel(repository = get())
+    }
+    factory {
+        val context = get() as Context
+        val resolver = context.contentResolver
+        val uploadDir = context.externalCacheDir ?: context.cacheDir
+        ImageGalleryPictureFinder(uploadDir, resolver) as FindHotelPicture
+    }
+    single {
         val logging = HttpLoggingInterceptor()
         logging.level = HttpLoggingInterceptor.Level.BODY
         val httpClient = OkHttpClient.Builder()
@@ -42,18 +60,15 @@ val androidModule = module{
 
     }
 
+
+
     factory {
-        HotelHttp(service = get(),
+        HotelHttp(
+            service = get(),
             repository = get(),
-            currentUser = "jordi")
+            pictureFinder = get(),
+            currentUser = "jordi"
+        )
     }
-    viewModel{
-        HotelListViewModel(repository = get())
-    }
-    viewModel {
-        HotelDetailsViewModel(repository = get())
-    }
-    viewModel {
-        HotelFormViewModel(repository = get())
-    }
+
 }
